872ea86d09f95137f473ee6a1c96d692370025c3
Роман Гольцман <romnasi@gmail.com>
2022-12-06 00:08:29 +0300

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
-const create = async () => {
-    // Write your code here 
+import { writeFile } from 'fs/promises';
+import getPathFromFile from '../utils/getPathFromFile.js';
+import { FS_ERROR_MESSAGE } from '../const/const.js';
+
+const TEXT_CONTENT = 'I am fresh and young';
+
+const filePath = getPathFromFile(import.meta.url, '/files/fresh.txt');
+
+export const create = async () => {
+  try {
+    await writeFile(filePath, TEXT_CONTENT, { flag: 'wx' });
+  } catch (err) {
+    throw new Error(FS_ERROR_MESSAGE);
+  }
 };
 
-await create();
+create();
