b417767d5fa8f9b5406a0d02dc582d86a5a15268
Роман Гольцман <romnasi@gmail.com>
2022-12-06 00:44:16 +0300

    feat: add function for env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
-const parseEnv = () => {
-    // Write your code here 
+// for test bash: RSS_name1=value1 RSS_name2=value2 node src/cli/env
+// for test powershell: $env:RSS_name1="value1"; $env:RSS_name2="value2"; node src/cli/env
+
+const FIND_PREFIX = 'RSS_';
+
+export const parseEnv = () => {
+  const rssEnvVars = Object.entries(process.env).reduce((acc, [key, value]) => {
+    if (key.startsWith(FIND_PREFIX)) {
+      acc.push(`${key}=${value}`)
+    }
+    return acc;
+  }, [])
+  console.log(rssEnvVars.join('; '));
 };
 
 parseEnv();
