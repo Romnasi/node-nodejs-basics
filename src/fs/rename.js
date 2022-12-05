42e335c9097bd202a95f40c1888ac4a9fef9462a
Роман Гольцман <romnasi@gmail.com>
2022-12-06 00:22:36 +0300

    feat: add rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,16 @@
-const rename = async () => {
-    // Write your code here 
+import { rename as fsRename } from 'node:fs';
+import getPathFromFile from '../utils/getPathFromFile.js';
+import { FS_ERROR_MESSAGE } from '../const/const.js';
+
+const wrongFilePath = getPathFromFile(import.meta.url, '/files/wrongFilename.txt');
+const filePath = getPathFromFile(import.meta.url, '/files/properFilename.md');
+
+const callback = (err) => {
+  if (err) throw new Error(FS_ERROR_MESSAGE);
+}
+
+export const rename = async () => {
+  fsRename(wrongFilePath, filePath, callback); 
 };
 
-await rename();
+rename();
