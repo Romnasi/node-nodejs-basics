b04f0f81dfa014521a72358ba177897ada7eb766
Роман Гольцман <romnasi@gmail.com>
2022-12-06 01:55:16 +0300

    feat: add spawnChildProcess function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { spawn } from 'node:child_process';
+import getPathFromFile from '../utils/getPathFromFile.js';
+
+const scriptPath = getPathFromFile(import.meta.url, '/files/script.js');
+
+export const spawnChildProcess = async (args) => {
+  spawn('node', args, {
+    stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
+  }).on('message', function(data) {
+    console.log(data);
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([scriptPath, 'first-argument', 'second-argument'])
