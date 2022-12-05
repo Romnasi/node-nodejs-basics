603216a43c4dd1affa3f385b74ea9f0d9b03bd21
Роман Гольцман <romnasi@gmail.com>
2022-12-06 00:16:23 +0300

    feat: add function copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,18 @@
-const copy = async () => {
-    // Write your code here 
+import { readdir, mkdir, copyFile } from 'fs/promises';
+import getPathFromFile from '../utils/getPathFromFile.js';
+import { FS_ERROR_MESSAGE } from '../const/const.js';
+
+const sourceFolder = getPathFromFile(import.meta.url, '/files');
+const destFolder = getPathFromFile(import.meta.url, '/files_copy');
+
+export const copy = async () => {
+  try {
+    await mkdir(destFolder);
+    const dirFiles = await readdir(sourceFolder);
+    await Promise.all(dirFiles.map((i) => copyFile(`${sourceFolder}/${i}`, `${destFolder}/${i}`)));
+  } catch (err) {
+    throw new Error(FS_ERROR_MESSAGE);
+  }
 };
 
-await copy();
+copy();
