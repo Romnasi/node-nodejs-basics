e6631df871d9d46f26654dd74e0fe6132658131c
Роман Гольцман <romnasi@gmail.com>
2022-12-06 01:08:58 +0300

    feat: update function to ESM

--- src/modules/cjsToEsm.mjs
+++ src/modules/cjsToEsm.mjs
@@ -1,7 +1,13 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path, { dirname } from 'path';
+import { release, version } from  'os';
+import { createServer as createServerHttp } from 'http';
+import { createRequire } from 'module';
+import { fileURLToPath } from 'url';
+import './files/c.js';
+
+const require = createRequire(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const random = Math.random();
 
@@ -20,21 +26,11 @@ console.log(`Path segment separator is "${path.sep}"`);
 console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
 
-const myServer = createServerHttp((_, res) => {
+const createMyServer = createServerHttp((_, res) => {
     res.end('Request accepted');
 });
 
-const PORT = 3000;
-
-console.log(unknownObject);
-
-myServer.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-    console.log('To terminate it, use Ctrl+C combination');
-});
-
-module.exports = {
+export {
     unknownObject,
-    myServer,
+    createMyServer,
 };
-
